3e294121a4da3c1f3cf2ac986e40b80a5a082a91
Ben Line <13621351+0xlino@users.noreply.github.com>
2023-12-18 01:10:39 +0000

    Refactor ask_inputs.py to include function documentation

--- ask_inputs.py
+++ ask_inputs.py
@@ -1,8 +1,23 @@
-# num_wallets = int(input("How many wallets to generate: "))
-# filename = input("Enter the file name: ")
-
 def create_ask_input_init(message):
+    """
+    This function is used to ask the user for input.
+
+    Parameters:
+    - message (str): The message to display when asking for input.
+
+    Returns:
+    - int: The user's input as an integer.
+    """
     return int(input(message))
 
 def create_ask_input(message):
+    """
+    This function is used to ask the user for input.
+
+    Parameters:
+    - message (str): The message to display when asking for input.
+
+    Returns:
+    - str: The user's input.
+    """
     return input(message)
