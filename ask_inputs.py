de8dbbabb927123aa00383bf14e1baa0c9cf96e1
Ben Line <13621351+0xlino@users.noreply.github.com>
2023-12-18 00:59:19 +0000

    Add functions to ask for user inputs

---
+++ ask_inputs.py
@@ -0,0 +1,8 @@
+# num_wallets = int(input("How many wallets to generate: "))
+# filename = input("Enter the file name: ")
+
+def create_ask_input_init(message):
+    return int(input(message))
+
+def create_ask_input(message):
+    return input(message)
